6387b5158945d29f7c0acb0e2f911e081144e455
alvdelci <delcimariosantos@gmail.com>
2022-05-13 11:59:19 -0300

    Adicionando novas funções

--- trabalho1/main.cpp
+++ trabalho1/main.cpp
@@ -2,48 +2,91 @@
 using namespace std;
 
 // Protótipos
-void inserir(float v[], float n, int &pos, int max);
-void inserirSemRepetir(float v[], float n, int &pos, int max);
-void listar(float v[], int quantidade);
-int buscar(float v[], int quantidade, int n);
-void remover(float v[], int &quantidade, int n);
-void intercalar(float v1[], float v2[], float v3[], int tamanho);
-void intersecao(float v1[], float v2[], float v3[], int tamanho);
-void uniao(float v1[], float v2[], float v3[], int tamanho);
+void inserir(float v[], float n, int &pos, int max);           // OK
+void inserirSemRepetir(float v[], float n, int &pos, int max); // OK
+void listar(float v[], int quantidade);                        // OK
+int buscar(float v[], int quantidade, int n);                  // OK
+void remover(float v[], int &quantidade, int n);               // OK
+void intercalar(float v1[], int tamanhoV1, float v2[], int tamanhoV2, float v3[], int &tamanhoV3);
+void intersecao(float v1[], int tamanhoV1, float v2[], int tamanhoV2, float v3[], int &tamanhoV3); // OK
+void uniao(float v1[], int tamanhoV1, float v2[], int tamanhoV2, float v3[], int &tamanhoV3);      // OK
 
 // Programa Principal
 int main(void)
 {
-    // Declaração da lista de notas
-    float lista1[10];
-    float lista2[10];
+    // Declaração da lista
+    float lista_entrada1[10];
+    float lista_entrada2[10];
 
-    int quantidade;
-    // Valor a ser lido
-    float n;
+    // lista dos valores das operacoes
+    float lista_intercalada[20], lista_intersecao[10], lista_uniao[20];
+
+    // quantidade de elementos nas lista
+    int quantidade_lista_entrada1 = 0, quantidade_lista_entrada2 = 0,
+        quantidade_lista_intercalada = 0, quantidade_lista_intersecao = 0,
+        quantidade_lista_uniao = 0;
+
     // Posição do resultado
     int pos;
 
-    // Inicializa a Lista: contém, inicialmente, zero elementos
-    quantidade = 0;
-
     // Leitura de N dado
+    int n;
     do
     {
-        int option;
         cout << "Enter a option:" << endl
              << "1 - Criar listas" << endl
              << "2 - Intercalar listas" << endl
-             << "3 - Fazer interseção das listas" << endl
+             << "3 - Fazer intersecao das listas" << endl
              << "4 - Unir listas" << endl
              << "5 - Remover" << endl
              << "0 - Sair" << endl;
-        cin >> option;
-        switch (option)
+        cin >> n;
+        switch (n)
         {
         case 1:
             cout << "Executa criar listas" << endl
                  << endl;
+
+            float input;
+            bool troca_lista;
+            bool fim_lista2;
+            do
+            {
+                fim_lista2 = false;
+
+                cout << "Insira valores na lista: " << endl
+                     << "Insira 0 para sair e < 0 para trocar de lista" << endl;
+                cin >> input;
+
+                if (input < 0)
+                {
+                    troca_lista = true;
+                }
+
+                if (!troca_lista)
+                {
+                    // Insere valores sem repetir na lista 1
+                    inserirSemRepetir(lista_entrada1, input, quantidade_lista_entrada1, 10);
+                    cout << "Inseriu na 1" << endl;
+                }
+                else if (troca_lista && input > 0)
+                {
+                    // insere valores sem repetir a lista 2
+                    inserirSemRepetir(lista_entrada2, input, quantidade_lista_entrada2, 10);
+                    cout << "inseriu na 2" << endl;
+                }
+                else if (input < 0 && quantidade_lista_entrada2 > 0)
+                {
+                    fim_lista2 = true;
+                }
+
+            } while (!fim_lista2);
+
+            cout << "lista 1" << endl;
+            listar(lista_entrada1, quantidade_lista_entrada1);
+            cout << "lista 2" << endl;
+            listar(lista_entrada2, quantidade_lista_entrada2);
+
             break;
         case 2:
             cout << "Executa Intercalar listas" << endl
@@ -51,14 +94,19 @@ int main(void)
             ;
             break;
         case 3:
-            cout << "Executa Insterseção" << endl
-                 << endl;
-            ;
+            intersecao(lista_entrada1, quantidade_lista_entrada1, lista_entrada2, quantidade_lista_entrada2, lista_intersecao, quantidade_lista_intersecao);
+
+            cout << endl
+                 << "Lista de Intersecoes" << endl;
+            listar(lista_intersecao, quantidade_lista_intersecao);
             break;
         case 4:
-            cout << "Executa Unir listas" << endl
-                 << endl;
-            ;
+            uniao(lista_entrada1, quantidade_lista_entrada1, lista_entrada2, quantidade_lista_entrada2, lista_uniao, quantidade_lista_uniao);
+
+            cout << endl
+                 << "Lista de Uniao" << endl;
+
+            listar(lista_uniao, quantidade_lista_uniao);
             break;
         case 5:
             cout << "Executa Remover" << endl
@@ -150,16 +198,43 @@ void remover(float v[], int &quantidade, int n)
 }
 
 // Recebe duas listas e as intercala
-void intercalar(float v1[], float v2[], float v3, int tamanho)
+void intercalar(float v1[], int tamanhoV1, float v2[], int tamanhoV2, float v3[], int &tamanhoV3)
 {
+    for (int element = 0; element < tamanhoV1 + tamanhoV2; element++)
+    {
+    }
 }
 
 // Recebe duas listas e constroi uum lista com os valores em comum nas duas
-void intersecao(float v1[], float v2[], float v3, int tamanho)
+void intersecao(float v1[], int tamanhoV1, float v2[], int tamanhoV2, float v3[], int &tamanhoV3)
 {
+    for (int element_lista1 = 0; element_lista1 < tamanhoV1; element_lista1++)
+    {
+        for (int element_lista2 = 0; element_lista2 < tamanhoV1; element_lista2++)
+        {
+            if (v1[element_lista1] == v2[element_lista2])
+            {
+                v3[tamanhoV3] = v1[element_lista1];
+                tamanhoV3++;
+            }
+        }
+    }
 }
 
 // Recebe as duas listas e as une
-void uniao(float v1[], float v2[], float v3, int tamanho)
+void uniao(float v1[], int tamanhoV1, float v2[], int tamanhoV2, float v3[], int &tamanhoV3)
 {
+    // Insere os valores do vetor 1 no vetor união
+    for (int element = 0; element < tamanhoV1; element++)
+    {
+        v3[element] = v1[element];
+        tamanhoV3++;
+    }
+
+    // insere os valores do vetor 2 no vetor união
+    for (int element = 0; element < tamanhoV2; element++)
+    {
+        v3[tamanhoV1 + element] = v2[element];
+        tamanhoV3++;
+    }
 }
